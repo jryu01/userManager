4c11b29b25be47ca042b3662b5f8b2a22f684765
Jaehwan Ryu <time0121@gmail.com>
2013-11-18 19:44:40 -0500

    allow cross origin ajax

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ if ('development' == app.get('env')) {
 //app.get('/', routes.index);
 
 //Restfull API Routes
+app.all('/api/*', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Content-Type", "application/json");
+  next();
+});
 app.get('/api/users', user.list);
 app.get('/api/users/:id', user.detail);
 app.post('/api/users', user.create);
