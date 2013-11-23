f9e992146488be492a8aef05bb8df97f8900b052
Jaehwan Ryu <time0121@gmail.com>
2013-11-23 16:02:47 -0500

    Access Control Allow Headers Content-Type added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if ('development' == app.get('env')) {
 //Restfull API Routes
 app.all('/api/*', function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   res.header("Content-Type", "application/json");
   next();
 });
