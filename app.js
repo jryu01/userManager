249fb5b4cb442364afdc6189ea1dfd8e9d91d533
Jaehwan Ryu <time0121@gmail.com>
2014-01-23 20:59:04 -0500

    h

--- app.js
+++ app.js
@@ -51,8 +51,7 @@ if ('development' == app.get('env')) {
 app.all('/api/*', function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-	//res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Methods: OPTIONS');
+	res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
   res.header("Content-Type", "application/json");
   next();
 });
