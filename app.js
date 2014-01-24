1463cdbef87f798dec4eb884c5c079af0da0e522
Jaehwan Ryu <time0121@gmail.com>
2014-01-23 20:47:14 -0500

    .

--- app.js
+++ app.js
@@ -48,12 +48,14 @@ if ('development' == app.get('env')) {
 //app.get('/', routes.index);
 
 //Restfull API Routes
-//app.all('/api/*', function(req, res, next){
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  //res.header("Content-Type", "application/json");
-  //next();
-//});
+app.all('/api/*', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+	//res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Methods: OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Content-Type", "application/json");
+  next();
+});
 
 app.get('/api/users', user.list);
 app.get('/api/users/:id', user.detail);
