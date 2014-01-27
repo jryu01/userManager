238bf6178f00f4f79d3bc76b52f78690c90ced42
Jaehwan Ryu <time0121@gmail.com>
2014-01-27 17:32:22 -0500

    index.html added

--- app.js
+++ app.js
@@ -45,8 +45,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-
 app.all('/api/*', function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
